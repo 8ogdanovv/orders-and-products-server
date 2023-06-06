068f1d44b72919eead1b6ea119700bccad7b5565
vadym4che <vadym4che@gmail.com>
2023-06-06 18:45:40 +0300

    implement server version 10

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 8080;
+const uri = 'mongodb+srv://vadym4che:0i142857@cluster0.umin0i5.mongodb.net/mydatabase?retryWrites=true&w=majority';
 
 app.use(cors());
 app.use(express.json());
 
-const dataDirectory = path.join(__dirname, 'data');
+async function connectToMongo() {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-// Load data from JSON files
-const readDataFile = (filename) => {
-  const filePath = path.join(dataDirectory, filename);
-  const rawData = fs.readFileSync(filePath, 'utf8');
-  return JSON.parse(rawData);
-};
-
-// Save data to JSON files
-const writeDataFile = (filename, data) => {
-  const filePath = path.join(dataDirectory, filename);
-  const jsonData = JSON.stringify(data, null, 2);
-  fs.writeFileSync(filePath, jsonData, 'utf8');
-};
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db('mydatabase');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('Express JS on Vercel');
@@ -33,15 +30,16 @@ app.get('/ping', (req, res) => {
   res.send('pong 🏓');
 });
 
-app.get('/orders', (req, res) => {
-  const orders = readDataFile('orders.json');
+app.get('/orders', async (req, res) => {
+  const db = await connectToMongo();
+  const orders = await db.collection('orders').find().toArray();
   res.json(orders);
 });
 
-app.get('/orders/:id', (req, res) => {
+app.get('/orders/:id', async (req, res) => {
   const orderId = parseInt(req.params.id);
-  const orders = readDataFile('orders.json');
-  const order = orders.find((order) => order.id === orderId);
+  const db = await connectToMongo();
+  const order = await db.collection('orders').findOne({ id: orderId });
 
   if (order) {
     res.json(order);
@@ -50,31 +48,30 @@ app.get('/orders/:id', (req, res) => {
   }
 });
 
-app.post('/orders', (req, res) => {
+app.post('/orders', async (req, res) => {
   const newOrder = req.body;
-  const orders = readDataFile('orders.json');
+  const db = await connectToMongo();
 
   // Generate a unique ID for the new order
-  const maxId = Math.max(...orders.map((order) => order.id));
-  newOrder.id = maxId + 1;
+  const maxId = await db.collection('orders').countDocuments() + 1;
+  newOrder.id = maxId;
 
-  orders.push(newOrder);
-  writeDataFile('orders.json', orders);
+  await db.collection('orders').insertOne(newOrder);
 
   res.json(newOrder);
 });
 
-app.patch('/orders/:id', (req, res) => {
+app.patch('/orders/:id', async (req, res) => {
   const orderId = parseInt(req.params.id);
-  const orders = readDataFile('orders.json');
-  const order = orders.find((order) => order.id === orderId);
+  const db = await connectToMongo();
+  const order = await db.collection('orders').findOne({ id: orderId });
 
   if (order) {
     // Handle the patch request to edit the order
     // You can access the request body using req.body
     // Implement your logic here
 
-    writeDataFile('orders.json', orders);
+    await db.collection('orders').updateOne({ id: orderId }, { $set: req.body });
 
     res.json(order);
   } else {
@@ -82,30 +79,28 @@ app.patch('/orders/:id', (req, res) => {
   }
 });
 
-app.delete('/orders/:id', (req, res) => {
+app.delete('/orders/:id', async (req, res) => {
   const orderId = parseInt(req.params.id);
-  const orders = readDataFile('orders.json');
-  const orderIndex = orders.findIndex((order) => order.id === orderId);
+  const db = await connectToMongo();
+  const result = await db.collection('orders').deleteOne({ id: orderId });
 
-  if (orderIndex !== -1) {
-    const deletedOrder = orders.splice(orderIndex, 1);
-    writeDataFile('orders.json', orders);
-
-    res.json(deletedOrder);
+  if (result.deletedCount === 1) {
+    res.json({ success: true });
   } else {
     res.status(404).json({ error: 'Order not found' });
   }
 });
 
-app.get('/products', (req, res) => {
-  const products = readDataFile('products.json');
+app.get('/products', async (req, res) => {
+  const db = await connectToMongo();
+  const products = await db.collection('products').find().toArray();
   res.json(products);
 });
 
-app.get('/products/:id', (req, res) => {
+app.get('/products/:id', async (req, res) => {
   const productId = parseInt(req.params.id);
-  const products = readDataFile('products.json');
-  const product = products.find((product) => product.id === productId);
+  const db = await connectToMongo();
+  const product = await db.collection('products').findOne({ id: productId });
 
   if (product) {
     res.json(product);
@@ -114,31 +109,30 @@ app.get('/products/:id', (req, res) => {
   }
 });
 
-app.post('/products', (req, res) => {
+app.post('/products', async (req, res) => {
   const newProduct = req.body;
-  const products = readDataFile('products.json');
+  const db = await connectToMongo();
 
   // Generate a unique ID for the new product
-  const maxId = Math.max(...products.map((product) => product.id));
-  newProduct.id = maxId + 1;
+  const maxId = await db.collection('products').countDocuments() + 1;
+  newProduct.id = maxId;
 
-  products.push(newProduct);
-  writeDataFile('products.json', products);
+  await db.collection('products').insertOne(newProduct);
 
   res.json(newProduct);
 });
 
-app.patch('/products/:id', (req, res) => {
+app.patch('/products/:id', async (req, res) => {
   const productId = parseInt(req.params.id);
-  const products = readDataFile('products.json');
-  const product = products.find((product) => product.id === productId);
+  const db = await connectToMongo();
+  const product = await db.collection('products').findOne({ id: productId });
 
   if (product) {
     // Handle the patch request to edit the product
     // You can access the request body using req.body
     // Implement your logic here
 
-    writeDataFile('products.json', products);
+    await db.collection('products').updateOne({ id: productId }, { $set: req.body });
 
     res.json(product);
   } else {
@@ -146,13 +140,15 @@ app.patch('/products/:id', (req, res) => {
   }
 });
 
-app.delete('/products/:index', (req, res) => {
+app.delete('/products/:index', async (req, res) => {
   const productIndex = parseInt(req.params.index);
-  const products = readDataFile('products.json');
+  const db = await connectToMongo();
+  const products = await db.collection('products').find().toArray();
 
   if (productIndex >= 0 && productIndex < products.length) {
     const deletedProduct = products.splice(productIndex, 1);
-    writeDataFile('products.json', products);
+    await db.collection('products').deleteMany({});
+    await db.collection('products').insertMany(products);
 
     res.json(deletedProduct);
   } else {

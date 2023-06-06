d87e029af849c186bf2b4f253da4aedfb41806d5
vadym4che <vadym4che@gmail.com>
2023-06-06 14:51:06 +0300

    implement server version 8 + fs + restructured

--- data.js
+++ data.js
@@ -5,7 +5,19 @@ export const orders = [
     date: '2017-06-29 12:09:33',
     group: 'Длинное предлинное длиннючее название группьі',
     owner: null,
-    get products () { return products.filter(p => p.order === this.id) },
+    products: {
+      id: 1,
+      quntity: 2,
+      isNew: 1,
+      guarantee: {
+        start: '2017-06-29 12:09:33',
+        end: '2017-06-29 12:09:33',
+      },
+      price: [
+        {value: 200, symbol: 'USD', isDefault: 0},
+        {value: 5200, symbol: 'UAH', isDefault: 1},
+      ]
+    },
   },
   {
     id: 2,
@@ -13,7 +25,19 @@ export const orders = [
     date: '2017-06-29 12:09:33',
     group: 'Длинное предлинное длиннючее название группьі',
     owner: null,
-    get products () { return products.filter(p => p.order === this.id) },
+    products: {
+      id: 1,
+      quntity: 2,
+      isNew: 0,
+      guarantee: {
+        start: '2017-06-29 12:09:33',
+        end: '2017-06-29 12:09:33',
+      },
+      price: [
+        {value: 2, symbol: 'USD', isDefault: 0},
+        {value: 50, symbol: 'UAH', isDefault: 1},
+      ],
+    },
   },
   {
     id: 3,
@@ -21,7 +45,19 @@ export const orders = [
     date: '2017-06-29 12:09:33',
     group: null,
     owner: 'Христорождественский Александр',
-    get products () { return products.filter(p => p.order === this.id) },
+    products: {
+      id: 1,
+      quntity: 2,
+      isNew: 1,
+      guarantee: {
+        start: '2017-06-29 12:09:33',
+        end: '2017-06-29 12:09:33',
+      },
+      price: [
+        {value: 100, symbol: 'USD', isDefault: 0},
+        {value: 2600, symbol: 'UAH', isDefault: 1},
+      ],
+    },
   },
   {
     id: 4,
@@ -29,7 +65,19 @@ export const orders = [
     date: '2017-06-29 12:09:33',
     group: 'Длинное предлинное длиннючее название группьі',
     owner: null,
-    get products () { return products.filter(p => p.order === this.id) },
+    products: {
+      id: 1,
+      quntity: 1,
+      isNew: 0,
+      guarantee: {
+        start: '2017-06-29 12:09:33',
+        end: '2017-06-29 12:09:33',
+      },
+      price: [
+        {value: 2, symbol: 'USD', isDefault: 0},
+        {value: 50, symbol: 'UAH', isDefault: 1},
+      ],
+    },
   },
 ]
 
@@ -37,96 +85,13 @@ export const products = [
   {
     id: 1,
     serialNumber: 1234,
-    isNew: 1,
-    photo: 'shopping.webp',
+    photo: 'https://static.bhphoto.com/images/multiple_images/images500x500/1657721733_IMG_1794925.jpg',
     title: 'Gigabyte M27F A 27" 165 Hz KVM Gaming Monitor',
     type: 'Monitors',
     specification: 'Specification 1',
-    guarantee: {
-      start: '2017-06-29 12:09:33',
-      end: '2017-06-29 12:09:33',
-    },
     price: [
       {value: 100, symbol: 'USD', isDefault: 0},
       {value: 2600, symbol: 'UAH', isDefault: 1},
     ],
-    order: 1,
-    date: '2017-06-29 12:09:33',
-  },
-  {
-    id: 1,
-    serialNumber: 1234,
-    isNew: 1,
-    photo: 'shopping.webp',
-    title: 'Gigabyte M27F A 27" 165 Hz KVM Gaming Monitor',
-    type: 'Monitors',
-    specification: 'Specification 1',
-    guarantee: {
-      start: '2017-06-29 12:09:33',
-      end: '2017-06-29 12:09:33',
-    },
-    price: [
-      {value: 100, symbol: 'USD', isDefault: 0},
-      {value: 2600, symbol: 'UAH', isDefault: 1},
-    ],
-    order: 1,
-    date: '2017-06-29 12:09:33',
-  },
-  {
-    id: 1,
-    serialNumber: 1234,
-    isNew: 0,
-    photo: 'shopping.webp',
-    title: 'Gigabyte M27F A 27" 165 Hz KVM Gaming Monitor',
-    type: 'Monitors',
-    specification: 'Specification 1',
-    guarantee: {
-      start: '2017-06-29 12:09:33',
-      end: '2017-06-29 12:09:33',
-    },
-    price: [
-      {value: 100, symbol: 'USD', isDefault: 0},
-      {value: 2600, symbol: 'UAH', isDefault: 1},
-    ],
-    order: 2,
-    date: '2017-06-29 12:09:33',
-  },
-  {
-    id: 1,
-    serialNumber: 1234,
-    isNew: 1,
-    photo: 'shopping.webp',
-    title: 'Gigabyte M27F A 27" 165 Hz KVM Gaming Monitor',
-    type: 'Monitors',
-    specification: 'Specification 1',
-    guarantee: {
-      start: '2017-06-29 12:09:33',
-      end: '2017-06-29 12:09:33',
-    },
-    price: [
-      {value: 100, symbol: 'USD', isDefault: 0},
-      {value: 2600, symbol: 'UAH', isDefault: 1},
-    ],
-    order: 3,
-    date: '2017-06-29 12:09:33',
-  },
-  {
-    id: 1,
-    serialNumber: 1234,
-    isNew: 0,
-    photo: 'shopping.webp',
-    title: 'Gigabyte M27F A 27" 165 Hz KVM Gaming Monitor',
-    type: 'Monitors',
-    specification: 'Specification 1',
-    guarantee: {
-      start: '2017-06-29 12:09:33',
-      end: '2017-06-29 12:09:33',
-    },
-    price: [
-      {value: 100, symbol: 'USD', isDefault: 0},
-      {value: 2600, symbol: 'UAH', isDefault: 1},
-    ],
-    order: 4,
-    date: '2017-06-29 12:09:33',
   }
 ]
--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 const express = require('express');
 const cors = require('cors');
-// Import the data module
-const { orders, products } = require('./data');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
-
-// Replace 'http://localhost:5173' with the actual origin of your frontend app
-// app.use(cors({ origin: 'http://localhost:5173' }));
-
-// JSON body parsing middleware
 app.use(express.json());
 
+const dataDirectory = path.join(__dirname, 'data');
+
+// Load data from JSON files
+const readDataFile = (filename) => {
+  const filePath = path.join(dataDirectory, filename);
+  const rawData = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(rawData);
+};
+
+// Save data to JSON files
+const writeDataFile = (filename, data) => {
+  const filePath = path.join(dataDirectory, filename);
+  const jsonData = JSON.stringify(data, null, 2);
+  fs.writeFileSync(filePath, jsonData, 'utf8');
+};
+
 app.get('/', (req, res) => {
-  res.send('Express JS on Vercel')
-})
+  res.send('Express JS on Vercel');
+});
 
 app.get('/ping', (req, res) => {
-  res.send('pong 🏓')
-})
+  res.send('pong 🏓');
+});
 
-// Define the routes
 app.get('/orders', (req, res) => {
+  const orders = readDataFile('orders.json');
   res.json(orders);
 });
 
 app.get('/orders/:id', (req, res) => {
   const orderId = parseInt(req.params.id);
+  const orders = readDataFile('orders.json');
   const order = orders.find((order) => order.id === orderId);
 
   if (order) {
@@ -37,14 +50,31 @@ app.get('/orders/:id', (req, res) => {
   }
 });
 
+app.post('/orders', (req, res) => {
+  const newOrder = req.body;
+  const orders = readDataFile('orders.json');
+
+  // Generate a unique ID for the new order
+  const maxId = Math.max(...orders.map((order) => order.id));
+  newOrder.id = maxId + 1;
+
+  orders.push(newOrder);
+  writeDataFile('orders.json', orders);
+
+  res.json(newOrder);
+});
+
 app.patch('/orders/:id', (req, res) => {
   const orderId = parseInt(req.params.id);
+  const orders = readDataFile('orders.json');
   const order = orders.find((order) => order.id === orderId);
 
   if (order) {
     // Handle the patch request to edit the order
     // You can access the request body using req.body
     // Implement your logic here
+
+    writeDataFile('orders.json', orders);
 
     res.json(order);
   } else {
@@ -54,10 +84,13 @@ app.patch('/orders/:id', (req, res) => {
 
 app.delete('/orders/:id', (req, res) => {
   const orderId = parseInt(req.params.id);
+  const orders = readDataFile('orders.json');
   const orderIndex = orders.findIndex((order) => order.id === orderId);
 
   if (orderIndex !== -1) {
     const deletedOrder = orders.splice(orderIndex, 1);
+    writeDataFile('orders.json', orders);
+
     res.json(deletedOrder);
   } else {
     res.status(404).json({ error: 'Order not found' });
@@ -65,11 +98,13 @@ app.delete('/orders/:id', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  const products = readDataFile('products.json');
   res.json(products);
 });
 
 app.get('/products/:id', (req, res) => {
   const productId = parseInt(req.params.id);
+  const products = readDataFile('products.json');
   const product = products.find((product) => product.id === productId);
 
   if (product) {
@@ -79,14 +114,31 @@ app.get('/products/:id', (req, res) => {
   }
 });
 
+app.post('/products', (req, res) => {
+  const newProduct = req.body;
+  const products = readDataFile('products.json');
+
+  // Generate a unique ID for the new product
+  const maxId = Math.max(...products.map((product) => product.id));
+  newProduct.id = maxId + 1;
+
+  products.push(newProduct);
+  writeDataFile('products.json', products);
+
+  res.json(newProduct);
+});
+
 app.patch('/products/:id', (req, res) => {
   const productId = parseInt(req.params.id);
+  const products = readDataFile('products.json');
   const product = products.find((product) => product.id === productId);
 
   if (product) {
     // Handle the patch request to edit the product
     // You can access the request body using req.body
     // Implement your logic here
+
+    writeDataFile('products.json', products);
 
     res.json(product);
   } else {
@@ -96,18 +148,18 @@ app.patch('/products/:id', (req, res) => {
 
 app.delete('/products/:index', (req, res) => {
   const productIndex = parseInt(req.params.index);
+  const products = readDataFile('products.json');
 
   if (productIndex >= 0 && productIndex < products.length) {
     const deletedProduct = products.splice(productIndex, 1);
+    writeDataFile('products.json', products);
+
     res.json(deletedProduct);
   } else {
     res.status(404).json({ error: 'Product not found' });
   }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-export default app;

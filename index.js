dc126e136cf1a4ced261240fefb5cfe511b7f6cb
vadym4che <vadym4che@gmail.com>
2023-06-11 17:19:42 +0300

    back to version without ws server

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
-const WebSocket = require('ws');
+// const WebSocket = require('ws');
 
-// Create a WebSocket server instance
-const wss = new WebSocket.Server({ port: 4444 });
+// // Create a WebSocket server instance
+// const wss = new WebSocket.Server({ port: 4444 });
 
-// Keep track of connected clients
-const clients = new Set();
+// // Keep track of connected clients
+// const clients = new Set();
 
-// Function to broadcast the instance count to all connected clients
-function broadcastInstanceCount() {
-  const instanceCount = clients.size;
-  for (const client of clients) {
-    client.send(instanceCount.toString());
-  }
-}
+// // Function to broadcast the instance count to all connected clients
+// function broadcastInstanceCount() {
+//   const instanceCount = clients.size;
+//   for (const client of clients) {
+//     client.send(instanceCount.toString());
+//   }
+// }
 
-// WebSocket connection event handler
-wss.on('connection', (ws) => {
-  // Add the new client to the set
-  clients.add(ws);
+// // WebSocket connection event handler
+// wss.on('connection', (ws) => {
+//   // Add the new client to the set
+//   clients.add(ws);
 
-  // Send the initial instance count to the client
-  ws.send(clients.size.toString());
+//   // Send the initial instance count to the client
+//   ws.send(clients.size.toString());
 
-  // WebSocket close event handler
-  ws.on('close', () => {
-    // Remove the client from the set
-    clients.delete(ws);
+//   // WebSocket close event handler
+//   ws.on('close', () => {
+//     // Remove the client from the set
+//     clients.delete(ws);
 
-    // Broadcast the updated instance count
-    broadcastInstanceCount();
-  });
-});
+//     // Broadcast the updated instance count
+//     broadcastInstanceCount();
+//   });
+// });
 
-// Start the server
-console.log('WebSocket server is running on port 4444.');
+// // Start the server
+// console.log('WebSocket server is running on port 4444.');
 
 const app = express();
 const port = process.env.PORT || 8080;
